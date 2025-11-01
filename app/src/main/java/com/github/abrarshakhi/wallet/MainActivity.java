aebe1fac441cbebe5d031c7b1c67b5807423c268
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2025-11-01 11:16:52 +0600

    init commit

---
+++ app/src/main/java/com/github/abrarshakhi/wallet/MainActivity.java
@@ -0,0 +1,52 @@
+package com.github.abrarshakhi.wallet;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.github.abrarshakhi.wallet.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        binding.bnvMainNavBar.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.bnbDetails) {
+                replaceFragment(new DetailsFragment());
+            } else if (item.getItemId() == R.id.bnbBudgets) {
+                replaceFragment(new BudgetsFragment());
+            }
+            return true;
+        });
+
+        replaceFragment(new DetailsFragment());
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.flMainFrameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+}
